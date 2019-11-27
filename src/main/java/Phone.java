c396462495cde6398f25bb842d5e20fff8ab2e34
Sıtkı <53573445+stkalagoz@users.noreply.github.com>
2019-11-28 00:14:46 +0300

    First project first commit with .gitignore

---
+++ src/main/java/Phone.java
@@ -0,0 +1,45 @@
+public class Phone {
+    private String name;
+    private String ram;
+    private Double price;
+    private String color;
+
+    public Phone(String name, String ram, Double price, String color) {
+        this.name = name;
+        this.ram = ram;
+        this.price = price;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
